4f3b2a1eac279e5ab7909f7449f68586c6e793f7
Rio Edwards <rioredwards@gmail.com>
2023-01-10 16:47:32 -0800

    Add component: zodiacs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,12 @@
 import './Main.css';
 import { zodiacs } from '../../data.js';
+import Zodiac from '../Zodiac/Zodiac.js';
 
 export default function Main() {
   return (
     <main>
       {zodiacs.map((zodiac) => (
-        <p className={zodiac.name} key={zodiac.id}>
-          {zodiac.name}
-        </p>
+        <Zodiac key={zodiac.id} name={zodiac.name} dates={zodiac.dates} symbol={zodiac.symbol} />
       ))}
     </main>
   );
